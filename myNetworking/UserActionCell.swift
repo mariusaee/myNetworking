a4dc8e90f859109410b474fe888eed06f6c03827
mariusaee <mariusae@me.com>
2021-03-04 21:23:10 +0300

    UserActiocCell class created

---
+++ myNetworking/UserActionCell.swift
@@ -0,0 +1,13 @@
+//
+//  UserActionCell.swift
+//  myNetworking
+//
+//  Created by Marius Malyshev on 04.03.2021.
+//
+
+import UIKit
+
+class UserActionCell: UICollectionViewCell {
+    
+    @IBOutlet var userActionLabel: UILabel!
+}
