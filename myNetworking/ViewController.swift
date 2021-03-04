2a88c2baa9900b87a9d5684f4b2ef29ea07739c5
mariusaee <mariusae@me.com>
2021-03-04 20:38:15 +0300

    Initial Commit

---
+++ myNetworking/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myNetworking
+//
+//  Created by Marius Malyshev on 04.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
