42798fd597c172e1b211275e98dacff25ef6048c
mariusaee <mariusae@me.com>
2021-03-04 22:09:04 +0300

    Перееопределил метод numberOfItemsInSection и cellForItemAt indexPath

--- myNetworking/MainViewController.swift
+++ myNetworking/MainViewController.swift
@@ -26,6 +26,15 @@ enum UserActions: String, CaseIterable {
 
 class MainViewController: UICollectionViewController {
 
+    // MARK: UICollectionViewDataSource
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        1
+    }
     
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! UserActionCell
+        
+        return cell
+    }
     
 }
