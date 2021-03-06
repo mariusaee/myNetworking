562cb09723825f0ff83c6178d8ef5705394fb9bd
mariusaee <mariusae@me.com>
2021-03-06 15:24:31 +0300

    add outlets in ImageViewController

--- myNetworking/ImageViewController.swift
+++ myNetworking/ImageViewController.swift
@@ -8,22 +8,12 @@
 import UIKit
 
 class ImageViewController: UIViewController {
-
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var activityIndicator: UIActivityIndicatorView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
